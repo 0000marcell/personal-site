9ae7413a88c6cd35a447707293fef70835073237
____marcell <0000marcell@gmail.com>
2020-06-21 21:03:27 +0000

    printing text yey!!!

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 extern crate cfg_if;
 extern crate wasm_bindgen;
+extern crate web_sys;
 
 mod utils;
 
@@ -18,15 +19,35 @@ cfg_if! {
 
 #[wasm_bindgen]
 pub struct Text {
+    value: String,
     x: u32,
     y: u32,
 }
 
 #[wasm_bindgen]
-pub fn main() {
-    let text = Text {
-        x: 10,
-        y: 10
-    };
-    
+impl Text {
+    pub fn new() -> Text {
+        web_sys::console::log_1(&"running!".into());
+        let x = 0;
+        let y = 48;
+        let value = "Hi, My name is Marcell".to_string();
+        Text {
+            value,
+            x,
+            y
+        }
+    }
+
+    pub fn value(&self) -> String {
+        self.value.clone().into()
+    }
+
+    pub fn x(&self) -> u32 {
+        self.x 
+    }
+
+    pub fn y(&self) -> u32 {
+        self.y 
+    }
 }
+
